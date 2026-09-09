877e21df15e8c9260991cd5666d3036f390e007c
darkapoparka <obi4amdar4eto@gmail.com>
2026-09-09 16:19:36 +0300

    feat(extra-car): add showroom route metadata

---
+++ clients/extra-car-sm/carwow/src/routes/about/showroom/+page.server.ts
@@ -0,0 +1,4 @@
+import { routeSeo } from '$lib/server/daynight-seo';
+import type { PageServerLoad } from './$types';
+export const prerender = true;
+export const load: PageServerLoad = async () => ({ seo: routeSeo('about/showroom') });
